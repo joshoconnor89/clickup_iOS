06dcdd1bc9542968374de5c5f53b34b9317ecddf
Josh O'Connor <josh.0@Joshs-Workstation.local>
2016-11-09 16:40:47 -0800

    First commit

---
+++ ClickUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickUp
+//
+//  Created by Joshua O'Connor on 11/9/16.
+//  Copyright © 2016 Mango Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
